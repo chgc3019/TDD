445a457de6a8ddad82d4be18a3da634e4f09d841
chgc3019 <110498190+chgc3019@users.noreply.github.com>
2024-06-23 22:03:12 -0500

    Add files via upload

---
+++ ClassLibraryCalculadora/Operaciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCalculadora
+{
+    public class Operaciones
+    {
+        public int Sumar(int nro1, int nro2)
+        {
+            return nro1 + nro2;
+        }
+        public int Restar(int nro1, int nro2)
+        {
+            return nro1 - nro2;
+        }
+        public int Multiplicar(int nro1, int nro2)
+        {
+            return nro1 * nro2;
+        }
+        public float Dividir(int nro1, int nro2)
+        {
+            return (float)nro1 / nro2;
+        }
+
+    }
+}
