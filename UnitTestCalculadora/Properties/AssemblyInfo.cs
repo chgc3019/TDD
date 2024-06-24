290a80c38ffa8fe623c818ac4e5440f0a62123da
chgc3019 <110498190+chgc3019@users.noreply.github.com>
2024-06-23 22:06:00 -0500

    Add files via upload

---
+++ UnitTestCalculadora/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCalculadora")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCalculadora")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70e1bd3b-14ed-4c99-a5c1-06558662e563")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestCalculadora/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ClassLibraryCalculadora;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCalculadora
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly int nro1 = 3;
+        private readonly int nro2 = 0;
+
+        Operaciones operacion = new Operaciones();
+
+        [TestMethod]
+        public void Sumar()
+        {
+            int resultado = operacion.Sumar(nro1, nro2);
+            Assert.AreEqual(31, resultado);
+            Console.WriteLine($@"{nro1} + {nro2}: {resultado} - Valor Esperado: 31");
+        }
+        
+        [TestMethod]
+        public void Restar()
+        {
+            int resultado = operacion.Restar(nro1, nro2);
+            Assert.AreEqual(30, resultado);
+            Console.WriteLine($@"{nro1} - {nro2}: {resultado} - Valor Esperado: 30");
+
+        }
+
+        [TestMethod]
+        public void Multiplicar()
+        {
+            int resultado = operacion.Multiplicar(nro1, nro2);
+            Assert.AreEqual(31, resultado);
+            Console.WriteLine($@"{nro1} * {nro2}: {resultado} - Valor Esperado: 31");
+        }
+
+        [TestMethod]
+        public void Dividir()
+        {
+            float resultado = operacion.Dividir(nro1, nro2);
+            Assert.AreEqual(3, resultado);
+            Console.WriteLine($@"{nro1} / {nro2}: {resultado} - Valor Esperado: 3");
+
+        }
+
+
+    }
+}
